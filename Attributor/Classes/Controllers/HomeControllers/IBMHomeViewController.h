14581b0c995edd3e50cffdff70bbbdabe3e98a7f
左穆 <zuomu.zdd@taobao.com>
2016-03-13 19:30:23 +0800

    add pch 文件

---
+++ Attributor/Classes/Controllers/HomeControllers/IBMHomeViewController.h
@@ -0,0 +1,13 @@
+//
+//  IBMHomeViewController.h
+//  Attributor
+//
+//  Created by 张栋栋 on 16/3/13.
+//  Copyright © 2016年 张栋栋. All rights reserved.
+//
+
+
+
+@interface IBMHomeViewController : UITabBarController<UITabBarControllerDelegate>
+
+@end
