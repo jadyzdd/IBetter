4e44b2ce7aa3eed8f9f176598dae5204dbef9e4b
左穆 <zuomu.zdd@taobao.com>
2016-03-13 17:13:31 +0800

    add new project

---
+++ Attributor/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Attributor
+//
+//  Created by 张栋栋 on 14-12-30.
+//  Copyright (c) 2014年 张栋栋. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
