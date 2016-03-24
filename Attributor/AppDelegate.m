919af69ed5bed244c6660091b8ba619883fd02f1
左穆 <zuomu.zdd@taobao.com>
2016-03-24 20:40:19 +0800

    hehe

--- Attributor/AppDelegate.m
+++ Attributor/AppDelegate.m
@@ -14,7 +14,7 @@
 
 @implementation AppDelegate
 
-//test commit lalla agian
+//test commit lalla agian agian
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
     self.window.backgroundColor = [UIColor whiteColor];
