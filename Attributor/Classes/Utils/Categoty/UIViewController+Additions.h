f180275b3d5dbd0d357fd211452cedb2ae0a085b
admin <jady@jadydembp.lan>
2016-11-05 11:55:34 +0800

    add folder

---
+++ Attributor/Classes/Utils/Categoty/UIViewController+Additions.h
@@ -0,0 +1,13 @@
+//
+//  UIViewController+Additions.h
+//  Attributor
+//
+//  Created by jady on 2016/11/5.
+//  Copyright © 2016年 张栋栋. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface UIViewController_Additions : NSObject
+
+@end
---
+++ Attributor/Classes/Utils/Categoty/UIViewController+Additions.m
@@ -0,0 +1,13 @@
+//
+//  UIViewController+Additions.m
+//  Attributor
+//
+//  Created by jady on 2016/11/5.
+//  Copyright © 2016年 张栋栋. All rights reserved.
+//
+
+#import "UIViewController+Additions.h"
+
+@implementation UIViewController_Additions
+
+@end
