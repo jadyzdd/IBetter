4c95268befa9546e55ae06712e1412f5b677e0c0
admin <jady@jadydeMacBook-Pro.local>
2017-02-10 01:22:05 +0800

    add file

---
+++ Attributor/Classes/NetWork/AnyNetwork.h
@@ -0,0 +1,13 @@
+//
+//  AnyNetwork.h
+//  Attributor
+//
+//  Created by jady on 2017/2/10.
+//  Copyright © 2017年 张栋栋. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface AnyNetwork : NSObject
+
+@end
---
+++ Attributor/Classes/NetWork/AnyNetwork.m
@@ -0,0 +1,13 @@
+//
+//  AnyNetwork.m
+//  Attributor
+//
+//  Created by jady on 2017/2/10.
+//  Copyright © 2017年 张栋栋. All rights reserved.
+//
+
+#import "AnyNetwork.h"
+
+@implementation AnyNetwork
+
+@end
